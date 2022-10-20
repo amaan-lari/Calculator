43c1f2eae35c003f98bb7cd930aaad14f1e53006
Yogesh Bansal <54579951+yogesh-7@users.noreply.github.com>
2022-10-21 01:08:29 +0530

    refactor the code to convert it into readable code

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 import javax.script.ScriptEngine
 import javax.script.ScriptEngineManager
@@ -13,7 +11,7 @@ import javax.script.ScriptException
 
 class MainActivity : AppCompatActivity() {
 
-    var check = 0
+    private var check = 0
 
     private lateinit var binding: ActivityMainBinding
 
@@ -31,156 +29,153 @@ class MainActivity : AppCompatActivity() {
             inputTextField.isActivated = true
             inputTextField.isPressed = true
 
-        button1.setOnClickListener {
-            text = inputTextField.text.toString()+"1"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button2.setOnClickListener {
-            text = inputTextField.text.toString()+"2"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button3.setOnClickListener {
-            text = inputTextField.text.toString()+"3"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button4.setOnClickListener {
-            text = inputTextField.text.toString()+"4"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button5.setOnClickListener {
-            text = inputTextField.text.toString()+"5"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button6.setOnClickListener {
-            text = inputTextField.text.toString()+"6"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button7.setOnClickListener {
-            text = inputTextField.text.toString()+"7"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button8.setOnClickListener {
-            text = inputTextField.text.toString()+"8"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button9.setOnClickListener {
-            text = inputTextField.text.toString()+"9"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button0.setOnClickListener {
-            text = inputTextField.text.toString()+"0"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        button00.setOnClickListener {
-            text = inputTextField.text.toString()+"00"
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        buttonDot.setOnClickListener {
-            text = inputTextField.text.toString()+"."
-            inputTextField.setText(text)
-            result(text)
-        }
-
-        // Operations buttons
-        buttonAdd.setOnClickListener {
-            text = inputTextField.text.toString()+"+"
-            inputTextField.setText(text)
-            check = check + 1
-        }
-
-        buttonSub.setOnClickListener {
-            text = inputTextField.text.toString()+"-"
-            inputTextField.setText(text)
-            check = check + 1
-        }
-
-        buttonMul.setOnClickListener {
-            text = inputTextField.text.toString()+"*"
-            inputTextField.setText(text)
-            check = check + 1
-        }
-
-        buttonDiv.setOnClickListener {
-            text = inputTextField.text.toString()+"/"
-            inputTextField.setText(text)
-            check = check + 1
-        }
-
-        buttonPercent.setOnClickListener {
-            text = inputTextField.text.toString()+"%"
-            inputTextField.setText(text)
-            check = check + 1
-        }
-
-        buttonEqual.setOnClickListener {
-            text = outputTextField.text.toString()+"+"
-            inputTextField.setText(text)
-            outputTextField.setText(null)
-        }
-
-        buttonAc.setOnClickListener {
-            inputTextField.setText(null)
-            outputTextField.setText(null)
-        }
-
-        buttonBackspace.setOnClickListener {
-            var backspace: String?= null
-            if (inputTextField.text.length > 0) {
-                val stringBuilder: StringBuilder = StringBuilder(inputTextField.text)
-                val find = inputTextField.text.toString();
-                val find2 = find.last()
-
-                if (find2.equals('+') || find2.equals('-') || find2.equals('*') || find2.equals('/') || find2.equals('%')) {
-                    check = check - 1
-                }
-
-                stringBuilder.deleteCharAt(inputTextField.text.length - 1)
-                backspace = stringBuilder.toString()
-                inputTextField.setText(backspace)
-                result(backspace)
+            button1.setOnClickListener {
+                text = inputTextField.text.toString() + "1"
+                inputTextField.setText(text)
+                result(text)
             }
-        }
+
+            button2.setOnClickListener {
+                text = inputTextField.text.toString() + "2"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button3.setOnClickListener {
+                text = inputTextField.text.toString() + "3"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button4.setOnClickListener {
+                text = inputTextField.text.toString() + "4"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button5.setOnClickListener {
+                text = inputTextField.text.toString() + "5"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button6.setOnClickListener {
+                text = inputTextField.text.toString() + "6"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button7.setOnClickListener {
+                text = inputTextField.text.toString() + "7"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button8.setOnClickListener {
+                text = inputTextField.text.toString() + "8"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button9.setOnClickListener {
+                text = inputTextField.text.toString() + "9"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button0.setOnClickListener {
+                text = inputTextField.text.toString() + "0"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            button00.setOnClickListener {
+                text = inputTextField.text.toString() + "00"
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            buttonDot.setOnClickListener {
+                text = inputTextField.text.toString() + "."
+                inputTextField.setText(text)
+                result(text)
+            }
+
+            // Operations buttons
+            buttonAdd.setOnClickListener {
+                text = inputTextField.text.toString() + "+"
+                inputTextField.setText(text)
+                check += 1
+            }
+
+            buttonSub.setOnClickListener {
+                text = inputTextField.text.toString() + "-"
+                inputTextField.setText(text)
+                check += 1
+            }
+
+            buttonMul.setOnClickListener {
+                text = inputTextField.text.toString() + "*"
+                inputTextField.setText(text)
+                check += 1
+            }
+
+            buttonDiv.setOnClickListener {
+                text = inputTextField.text.toString() + "/"
+                inputTextField.setText(text)
+                check += 1
+            }
+
+            buttonPercent.setOnClickListener {
+                text = inputTextField.text.toString() + "%"
+                inputTextField.setText(text)
+                check += 1
+            }
+
+            buttonEqual.setOnClickListener {
+                text = outputTextField.text.toString() + "+"
+                inputTextField.setText(text)
+                outputTextField.text = null
+            }
+
+            buttonAc.setOnClickListener {
+                inputTextField.text = null
+                outputTextField.text = null
+            }
+
+            buttonBackspace.setOnClickListener {
+                val backspace: String?
+                if (inputTextField.text.isNotEmpty()) {
+                    val stringBuilder: StringBuilder = StringBuilder(inputTextField.text)
+                    val find = inputTextField.text.toString()
+                    val find2 = find.last()
+
+                    if (find2 == '+' || find2 == '-' || find2 == '*' || find2 == '/' || find2 == '%'
+                    ) {
+                        check -= 1
+                    }
+
+                    stringBuilder.deleteCharAt(inputTextField.text.length - 1)
+                    backspace = stringBuilder.toString()
+                    inputTextField.setText(backspace)
+                    result(backspace)
+                }
+            }
         }
     }
 
-    private fun result(text: String) = with(binding){
+    private fun result(text: String) = with(binding) {
 
         val engine: ScriptEngine = ScriptEngineManager().getEngineByName("rhino")
         try {
             val result: Any = engine.eval(text)
-            var mainr = result.toString()
-            if (check == 0)
-            {
+            val mainr = result.toString()
+            if (check == 0) {
                 outputTextField.setText(null)
-            }
-            else
-            {
+            } else {
                 outputTextField.setText(mainr)
             }
-        }
-        catch (e:ScriptException) {
+        } catch (e: ScriptException) {
             Log.d("TAG", "ERROR")
         }
     }
